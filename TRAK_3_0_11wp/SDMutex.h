56f8c0fc51f4082cefd60bdaf0e5dc53b2b3d6e8
Zer0S-dev <maximestandford@gmail.com>
2026-09-07 10:31:01 +0200

    Delete TRAK_3_0_11wp directory

--- TRAK_3_0_11wp/SDMutex.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-
-// Mutex global de la carte SD partagee entre Core 0 (buffer/logs) et Core 1 (Web).
-bool sdMutexBegin();
-bool sdMutexLock(TickType_t timeoutTicks = portMAX_DELAY);
-void sdMutexUnlock();
--- TRAK_3_0_11wp/Secrets.h
+++
@@ -1,7 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-// Les identifiants Wi-Fi utilisateur ne sont plus embarqués dans le firmware.
-// Les 3 profils Wi-Fi sont enregistrés par l'utilisateur dans la NVS depuis le Dashboard.
-
-#endif
