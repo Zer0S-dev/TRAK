fbfba9938c5a3e9eb2836623fd15038db795176f
Zer0S-dev <maximestandford@gmail.com>
2026-09-10 15:58:35 +0200

    TRAK 3.1.0 phase 2: expose config console command handler

--- trak_3_0_4/TrakConfig.h
+++ trak_3_0_4/TrakConfig.h
@@ -6,6 +6,7 @@
 // Wi-Fi profiles remain independent in "trak_wifi".
 void trakConfigBegin();
 void trakConfigTask();
+void trakConfigHandleCommand(const String& command);
 
 String trakWebAppUrl();
 String trakApiKey();
@@ -14,7 +15,6 @@ String trakPhone();
 bool trakConfigProvisioned();
 bool trakConfigReady();
 
-// Update persistent provisioning values. These will be used by the Wizard.
 bool trakConfigSetServerUrl(const String& url);
 bool trakConfigSetUserPhone(const String& phone);
 bool trakConfigSetTrakPhone(const String& phone);
@@ -23,9 +23,3 @@ bool trakConfigRegenerateApiKey();
 
 // Erase only server/personal provisioning data. Never touches "trak_wifi".
 void trakConfigResetProvisioning();
-
-// USB/Serial provisioning commands kept for development:
-//   SETURL https://example.com/trak/
-//   SHOWCONFIG
-//   SHOWKEY
-//   RESETCONFIG
